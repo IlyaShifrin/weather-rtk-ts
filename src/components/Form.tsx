1acdf2374d49826bf1a8e2bf4d22109a26f35900
Ilya <drunktrumpet1@gmail.com>
2024-12-24 21:20:23 +0200

    First commit

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,14 @@
 import React, {useState} from "react";
+import {useAppDispatch} from "../app/hooks.ts";
+import {setCity} from "../features/slices/citySlice.ts";
 
-interface Props {
-    setCity: (city: string) => void;
-}
-
-const Form = ({setCity}: Props) => {
+const Form = () => {
     const [cityName, setCityName] = useState('');
+    const dispatch = useAppDispatch();
 
     const getCity = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        setCity(cityName);
+        dispatch(setCity(cityName));
         setCityName('');
     }
 
